c989af40d6eae342c16864f157c9726efb640ffc
kongjak777-afk <kongjak777@gmail.com>
2026-02-02 17:54:41 +0900

    시작

---
+++ src/main/java/com/example/foodfacts/FoodfactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodfactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodfactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodfacts/FoodfactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodfactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
